57228dc2fcce7ed9d7ea90550530a8119e0db72c
Kanishk Pareek <kanishkgit@gmail.com>
2024-11-15 23:23:36 +0530

    changes

---
+++ controllers/apiController.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+require('dotenv').config();
+
+const API_BASE_URL = process.env.API_BASE_URL;
+
+exports.createOrder = async (req, res) => {
+    try {
+        console.log(req.body)
+        const response = await axios.post(`https://allapi.in/order/create`, req.body, {
+            headers: {
+                'Content-Type': 'application/json',
+                // Add additional headers if needed
+            },
+        }).then((response)=>console.log(response.data)).catch((error) => console.error('Error:', error.message));
+        res.status(response.status).json(response.data);
+    } catch (error) {
+        console.log('Error:', error.message);
+        res.status(error.response?.status || 500).json({
+            error: error.message,
+            details: error.response?.data || 'Internal Server Error',
+        });
+    }
+};
